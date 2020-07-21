ab9c0fd78a5f601089b99211db8a874b03390959
Aniruddha Joshi <aniruddhajoshi@dhcp-10-105-76-254.wireless.northwestern.private>
2020-07-21 16:49:06 -0500

    Revert "Checking in Flask API and Web Dashboard"
    This reverts commit 113950be103abb66235949dd42a6ef1ea5471005.

--- Utilities/Main.py
+++ Utilities/Main.py
@@ -3,26 +3,25 @@ import numpy as np
 import time
 from PreprocessStocksData import *
 from LinearRegressionModelling import * 
-# from KnnModelling import *
-# from AutoArimaModelling import *
-# from LSTMModelling import *
-# from PlottingModels import *
+from KnnModelling import *
+from AutoArimaModelling import *
+from LSTMModelling import *
+from PlottingModels import *
 import json
 
-def prediction(ticker):
-    pd.options.mode.chained_assignment = None
-    ticker_stock_data= pd.DataFrame()
-    parameters={}
-    start= time.time()
-    ticker_stock_data=fetch_data_from_db(ticker)
-    ticker_stock_data=preprocess_ticker_data(ticker_stock_data)
-    parameters=split_data(ticker_stock_data)
-    parameters=model_linearRegression(parameters)
-    # parameters=model_knn(parameters)
-    # parameters=model_autoArima(parameters)
-    # parameters=model_LSTM(parameters)
-    # current_price(parameters)
-    # forecast_price(parameters)
-    json = convert_to_json(parameters)
-    print("\nTime to load data: {} seconds".format(time.time() - start))
-    return json
+pd.options.mode.chained_assignment = None
+ticker_stock_data= pd.DataFrame()
+parameters={}
+start= time.time()
+ticker_stock_data=fetch_data_from_db('GOOG')
+ticker_stock_data=preprocess_ticker_data(ticker_stock_data)
+parameters=split_data(ticker_stock_data)
+parameters=model_linearRegression(parameters)
+# parameters=model_knn(parameters)
+# parameters=model_autoArima(parameters)
+# parameters=model_LSTM(parameters)
+# current_price(parameters)
+# forecast_price(parameters)
+convert_to_json(parameters)
+
+print("\nTime to load data: {} seconds".format(time.time() - start))
